58c9a699e6d987b90237dd25d8d543b837f35fe0
John Neill <44747107+Johneill@users.noreply.github.com>
2020-10-21 11:27:40 +0100

    First app trail
    Needs some more work still

---
+++ hurry/app/src/main/java/com/example/hurry/MainActivity.java
@@ -0,0 +1,94 @@
+package com.example.hurry;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText editText;
+    Button button;
+
+    String playerName= "";
+
+    FirebaseDatabase database;
+    DatabaseReference playerRef;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        editText= findViewById(R.id.editText);
+        button = findViewById(R.id.button);
+
+        database = FirebaseDatabase.getInstance();
+        SharedPreferences preferences = getSharedPreferences("PREFS", 0);
+        playerName = preferences.getString("playerName", "");
+
+        if(!playerName.equals("")){
+            playerRef = database.getReference("players/"+ playerName);
+            addEventListener();
+            playerRef.setValue("");
+        }
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+            //logging the player in
+                playerName = editText.getText().toString();
+                editText.setText("");
+                if(!playerName.equals("")){
+                    button.setText("LOGGING IN");
+                    button.setEnabled(false);
+                    playerRef = database.getReference("players/"+playerName);
+                    addEventListener();
+                    playerRef.setValue("");
+                }
+            }
+        });
+    }
+
+    private void addEventListener(){
+        //read from database
+        playerRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                //success on continung to the next screen after saving player name
+                if(!playerName.equals("")){
+                    SharedPreferences preferences = getSharedPreferences("PREFS",0);
+                    SharedPreferences.Editor editor = preferences.edit();
+                    editor.putString("playerName", playerName);
+                    editor.apply();
+
+                    startActivity(new Intent(getApplicationContext(),MainActivity2.class));
+                    finish();
+                }
+            }
+
+            @Override
+            //if there is an error
+            public void onCancelled(@NonNull DatabaseError error) {
+                button.setText("Log in");
+                button.setEnabled(true);
+                Toast.makeText(MainActivity.this, "Error", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
+    }
+}
---
+++ hurry/app/src/main/java/com/example/hurry/MainActivity2.java
@@ -0,0 +1,142 @@
+package com.example.hurry;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity2 extends AppCompatActivity {
+
+
+
+    ListView listView;
+    Button button;
+
+    List<String> roomList;
+
+    String playerName = "";
+    String roomName = "";
+
+    FirebaseDatabase database;
+    DatabaseReference roomRef;
+    DatabaseReference roomsRef;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+
+        database = FirebaseDatabase.getInstance();
+        SharedPreferences preferences = getSharedPreferences("PREFS", 0);
+        playerName = preferences.getString("playerName", "");
+        roomName = playerName;
+
+        listView = findViewById(R.id.listview);
+        button = findViewById(R.id.button);
+
+        roomList = new ArrayList<>();
+
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                button.setText("Creating Room");
+                button.setEnabled(false);
+                roomName = playerName;
+                roomRef = database.getReference("rooms/" + roomName + "/player1");
+                addRoomEventListener();
+                roomRef.setValue(playerName);
+
+
+            }
+        });
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                roomName = roomList.get(position);
+                roomRef = database.getReference("rooms/" + roomName + "/player2");
+                addRoomEventListener();
+                roomRef.setValue(playerName);
+
+
+            }
+        });
+        addRoomsEventListener();
+    }
+
+    private void addRoomEventListener(){
+        roomRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                button.setText("Create Room");
+                button.setEnabled(true);
+                Intent intent = new Intent(getApplicationContext(), MainActivity3.class);
+                intent.putExtra("roomName",roomName);
+                startActivity(intent);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                button.setText("Create Room");
+                button.setEnabled(true);
+                Toast.makeText(MainActivity2.this, "ERROR", Toast.LENGTH_SHORT).show();
+
+            }
+        });
+
+
+
+    }
+
+    private void addRoomsEventListener(){
+
+       roomRef = database.getReference("rooms");
+       roomRef.addValueEventListener(new ValueEventListener() {
+           @Override
+           public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+               //show list of rooms
+
+               roomList.clear();
+               Iterable<DataSnapshot> rooms = dataSnapshot.getChildren();
+               for(DataSnapshot snapshot:rooms){
+                   roomList.add(snapshot.getKey());
+
+                   ArrayAdapter<String> adapter = new ArrayAdapter<>(MainActivity2.this,
+                           android.R.layout.simple_list_item_1,roomList);
+                   listView.setAdapter(adapter);
+               }
+           }
+
+           @Override
+           public void onCancelled(@NonNull DatabaseError error) {
+
+           }
+       });
+
+
+    }
+
+
+
+
+
+}
