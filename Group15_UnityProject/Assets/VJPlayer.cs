80887be5c262c24dab0cf8715851e5f914b406f5
alohanick <740400839@qq.com>
2020-10-23 16:05:09 +0800

    Add VirtualJoyStick and FireButtom

---
+++ Group15_UnityProject/Assets/VJPlayer.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class VJPlayer : MonoBehaviour
+{
+
+    public float moveSpeed = 05f;
+    public VJStick jsMovement;
+
+    private Vector3 direction;
+    private float xMin, xMax, yMin, yMax;
+
+    void Update()
+    {
+
+        direction = jsMovement.InputDirection; //InputDirection can be used as per the need of your project
+
+        if (direction.magnitude != 0)
+        {
+
+            transform.position += direction * moveSpeed;
+            transform.position = new Vector3(Mathf.Clamp(transform.position.x, xMin, xMax), Mathf.Clamp(transform.position.y, yMin, yMax), 0f);//to restric movement of player
+        }
+    }
+
+    void Start()
+    {
+
+        //Initialization of boundaries
+        xMax = Screen.width-35; // I used 50 because the size of player is 100*100
+        xMin = 35;
+        yMax = Screen.height-35 ;
+        yMin = 35;
+    }
+}
