bb9662c4afdd2cc1f38353c74a2c5fd54fd26272
alohanick <740400839@qq.com>
2020-10-29 17:10:43 +0800

    generalfunction

--- Group15_UnityProject/Assets/ButtomClick.cs
+++ Group15_UnityProject/Assets/ButtomClick.cs
@@ -1,4 +1,198 @@
-﻿using System.Collections;
+﻿//using System.Collections;
+//using System.Collections.Generic;
+//using UnityEngine;
+//using UnityEngine.Networking;
+//using Mirror;
+
+//public class ButtomClick : NetworkBehaviour
+//{
+//    public int i = 0;
+//    public GameObject TracePrefab;
+//    ButtonIsClicked Buttonflag;
+//    public bool ButtonState=false;
+//    public List<GameObject> Traces = new List<GameObject> ();
+
+//    void Update() {
+//        if (isLocalPlayer == false) {
+//            return;
+//        }
+//        Buttonflag = (ButtonIsClicked)GameObject.Find("Button").GetComponent("ButtonIsClicked");//this is a bool, if 
+//        if (Buttonflag.ButtonState)
+//        {
+//            bool Flag = CmdJudge();
+//            if (Flag)
+//            {
+//                CmdGenerateTrace();
+//                Flag = false;
+//            }
+
+//            Buttonflag.ButtonState = false;
+//        }
+
+//    }
+
+//    [Command]
+//        void CmdGenerateTrace() {
+//            GameObject Trace = Instantiate(TracePrefab, this.transform.position, this.transform.rotation) as GameObject;
+//            NetworkServer.Spawn(Trace);
+//            Traces.Add(Trace);
+//            i = i + 1;
+//    }
+
+//    //[Command]
+//    bool CmdJudge() {
+//        GameObject container = GameObject.Find("MovingTile");
+//        if (this.transform.position.x < container.transform.position.x + 92 && transform.position.x > container.transform.position.x - 92
+//       && transform.position.y < container.transform.position.y + 52 && transform.position.y > container.transform.position.y - 65)
+//        {
+//            for (int m = 0; m < i; m++)
+//            {
+//                if (this.transform.position.x <= Traces[m].transform.position.x + 20 && this.transform.position.x >= Traces[m].transform.position.x - 20
+//                    && this.transform.position.y <= Traces[m].transform.position.y + 20 && this.transform.position.y >= Traces[m].transform.position.y - 20)
+//                {
+//                    GeneralFailHint();
+//                    return false;
+//                }
+//            }
+//            GeneralSuccessHint();
+//            CmdGenerateTrace();
+//            return true;
+//        }
+//        else
+//        {
+//            GeneralMissHint();
+//            return false;
+
+//        }
+//    }
+
+//    void GeneralSuccessHint() {        
+//        GameObject Great = GameObject.Find("Great!!");
+//        GameObject GreatInstance = Instantiate(Great);
+//        GreatInstance.transform.position = new Vector2(190, 140);
+//        GreatInstance.transform.localScale = new Vector2(50, 50);
+//        Destroy(GreatInstance, 1.0f);
+//    }
+//    void GeneralMissHint()
+//    {
+//        GameObject Missed = GameObject.Find("Missed!!");
+//        GameObject MissedInstance = Instantiate(Missed);
+//        MissedInstance.transform.position = new Vector2(190, 140);
+//        MissedInstance.transform.localScale = new Vector2(50, 50);
+//        Destroy(MissedInstance, 1.0f);
+//    }
+
+//    void GeneralFailHint() {
+//        GameObject Failed = GameObject.Find("Failed!!");
+//        GameObject FailedInstance = Instantiate(Failed);
+//        FailedInstance.transform.position = new Vector2(190, 140);
+//        FailedInstance.transform.localScale = new Vector2(50, 50);
+//        Destroy(FailedInstance, 1.0f);
+//    }
+
+
+//}
+
+//using System.Collections;
+//using System.Collections.Generic;
+//using UnityEngine;
+//using UnityEngine.Networking;
+//using Mirror;
+
+//public class ButtomClick : NetworkBehaviour
+//{
+//    public int i = 0;
+//    public GameObject TracePrefab;
+//    ButtonIsClicked Buttonflag;
+//    public bool ButtonState = false;
+//    public List<GameObject> Traces = new List<GameObject>();
+//    void Update()
+//    {
+//        if (isLocalPlayer == false)
+//        {
+//            return;
+//        }
+//        Buttonflag = (ButtonIsClicked)GameObject.Find("Button").GetComponent("ButtonIsClicked");//this is a bool, if 
+//        if (Buttonflag.ButtonState)
+//        {
+//            bool Flag=Judge();
+//            if (Flag)
+//            {
+//                CmdGenerateTrace();
+//                GeneralSuccessHint();
+//                Flag = false;
+//            }
+//            else
+//            {
+//                GeneralFailHint();
+//            }
+//            Buttonflag.ButtonState = false;
+//        }
+
+//    }
+
+//    [Command]
+//    void CmdGenerateTrace()
+//    {
+//        GameObject Trace = Instantiate(TracePrefab, this.transform.position, this.transform.rotation) as GameObject;
+//        NetworkServer.Spawn(Trace);
+//        Traces.Add(Trace);
+//        i = i + 1;
+//    }
+
+//    bool Judge()
+//    {
+//        GameObject container = GameObject.Find("MovingTile");
+//        if (this.transform.position.x < container.transform.position.x + 92 && transform.position.x > container.transform.position.x - 92
+//       && transform.position.y < container.transform.position.y + 52 && transform.position.y > container.transform.position.y - 65)
+//        {
+//            for (int m = 0; m < i; m++)
+//            {
+//                if (this.transform.position.x <= Traces[m].transform.position.x + 20 && this.transform.position.x >= Traces[m].transform.position.x - 20
+//                    && this.transform.position.y <= Traces[m].transform.position.y + 20 && this.transform.position.y >= Traces[m].transform.position.y - 20)
+//                {
+//                    return false;
+//                }
+//            }
+//            return true;
+//        }
+//        else
+//        {
+//            return false;
+
+//        }
+//    }
+
+//    void GeneralSuccessHint()
+//    {
+//        GameObject Great = GameObject.Find("Great!!");
+//        GameObject GreatInstance = Instantiate(Great);
+//        GreatInstance.transform.position = new Vector2(190, 140);
+//        GreatInstance.transform.localScale = new Vector2(50, 50);
+//        Destroy(GreatInstance, 1.0f);
+//    }
+//    void GeneralMissHint()
+//    {
+//        GameObject Missed = GameObject.Find("Missed!!");
+//        GameObject MissedInstance = Instantiate(Missed);
+//        MissedInstance.transform.position = new Vector2(190, 140);
+//        MissedInstance.transform.localScale = new Vector2(50, 50);
+//        Destroy(MissedInstance, 1.0f);
+//    }
+
+//    void GeneralFailHint()
+//    {
+//        GameObject Failed = GameObject.Find("Failed!!");
+//        GameObject FailedInstance = Instantiate(Failed);
+//        FailedInstance.transform.position = new Vector2(190, 140);
+//        FailedInstance.transform.localScale = new Vector2(50, 50);
+//        Destroy(FailedInstance, 1.0f);
+//    }
+
+
+//}
+
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Networking;
@@ -8,95 +202,48 @@ public class ButtomClick : NetworkBehaviour
 {
     public int i = 0;
     public GameObject TracePrefab;
-    //public Transform TraceSpawn;
     ButtonIsClicked Buttonflag;
-    public bool ButtonState=false;
-    public float[] XArray = new float[20];
-    public float[] YArray = new float[20];
-    //public List<GameObject.transform.position> Positions = new List<GameObject.transform.position>();
-    public List<GameObject> Traces = new List<GameObject> ();
-    public GameObject MyTrace;
-
-    void Update() {
-        if (isLocalPlayer == false) {
+    public bool ButtonState = false;
+    public List<GameObject> Traces = new List<GameObject>();
+    void Update()
+    {
+        if (isLocalPlayer == false)
+        {
             return;
         }
         Buttonflag = (ButtonIsClicked)GameObject.Find("Button").GetComponent("ButtonIsClicked");//this is a bool, if 
         if (Buttonflag.ButtonState)
         {
-            bool Flag = Judge(XArray, YArray);
-            if (Flag)
-            {
-                CmdGenerateTrace();
-                
-                Flag = false;
-            }
+            CmdJudge();
             Buttonflag.ButtonState = false;
         }
-
     }
 
     [Command]
-    void CmdGenerateTrace() {
-            GameObject Trace = Instantiate(TracePrefab, this.transform.position, this.transform.rotation) as GameObject;
-            NetworkServer.Spawn(Trace);             
-            //Trace.transform.parent = GameObject.Find("MovingTile").transform;//The Problem Code(Make the Trace follow the Moving Tile)       
-            XArray[i] = this.transform.position.x;
-            YArray[i] = this.transform.position.y;
-            i = i + 1;
-            
-               
-    }
-
-    bool Judge(float[] XArray,float[] YArray) {
+    void CmdJudge()
+    {
+        //List<GameObject> Traces = TracesList;
         GameObject container = GameObject.Find("MovingTile");
         if (this.transform.position.x < container.transform.position.x + 92 && transform.position.x > container.transform.position.x - 92
        && transform.position.y < container.transform.position.y + 52 && transform.position.y > container.transform.position.y - 65)
         {
-            for (int m = 0; m <= i; m++)
+            for (int m = 0; m < i; m++)
             {
-                if (this.transform.position.x <= XArray[m] + 20 && this.transform.position.x >= XArray[m] - 20
-                    && this.transform.position.y <= YArray[m] + 20 && this.transform.position.y >= YArray[m] - 20)
+                if (this.transform.position.x <= Traces[m].transform.position.x + 20 && this.transform.position.x >= Traces[m].transform.position.x - 20
+                    && this.transform.position.y <= Traces[m].transform.position.y + 20 && this.transform.position.y >= Traces[m].transform.position.y - 20)
                 {
-                    GeneralFailHint();
-                    return false;
+                    return;
                 }
             }
-            GeneralSuccessHint();
-            return true;
+            GameObject Trace = Instantiate(TracePrefab, this.transform.position, this.transform.rotation) as GameObject;
+            NetworkServer.Spawn(Trace);
+            Traces.Add(Trace);
+            i = i + 1;
         }
         else
         {
-            GeneralMissHint();
-            return false;
-            
+            return;
         }
     }
 
-    void GeneralSuccessHint() {        
-        GameObject Great = GameObject.Find("Great!!");
-        GameObject GreatInstance = Instantiate(Great);
-        GreatInstance.transform.position = new Vector2(190, 140);
-        GreatInstance.transform.localScale = new Vector2(50, 50);
-        Destroy(GreatInstance, 1.0f);
-    }
-    void GeneralMissHint()
-    {
-        GameObject Missed = GameObject.Find("Missed!!");
-        GameObject MissedInstance = Instantiate(Missed);
-        MissedInstance.transform.position = new Vector2(190, 140);
-        MissedInstance.transform.localScale = new Vector2(50, 50);
-        Destroy(MissedInstance, 1.0f);
-    }
-
-    void GeneralFailHint() {
-        GameObject Failed = GameObject.Find("Failed!!");
-        GameObject FailedInstance = Instantiate(Failed);
-        FailedInstance.transform.position = new Vector2(190, 140);
-        FailedInstance.transform.localScale = new Vector2(50, 50);
-        Destroy(FailedInstance, 1.0f);
-    }
-
-
 }
-
---
+++ Group15_UnityProject/Assets/FollowTheTile.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowTheTile : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.transform.parent = GameObject.Find("MovingTile").transform;//The Problem Code(Make the Trace follow the Moving Tile)   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
